fbe5296aecdf3e7a15f03ca6066284bac65db8a1
Siraj <sirajwahaj.ibrahim@gmail.com>
2022-07-07 03:41:22 +0000

    Setting up movement commands

--- input.js
+++ input.js
@@ -1,21 +1,36 @@
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.resume();
-
   stdin.on("data", handleUserInput);
-  const handleUserInput = function() {
-    // your code here
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  };
-
+  stdin.resume();
   return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input, terminate the game
+    process.exit();
+  }
+  if (key === 'w') { // "W, moving up"
+    connection.write("Move: up");
+  }
+  if (key === 'a') { // "A, moving left"
+    connection.write("Move: left");
+  }
+  if (key === 's') { // "S, moving down"
+    connection.write("Move: down");
+  }
+  if (key === 'd') { // "D, moving right"
+    connection.write("Move: right");
+  }
+
+  //Say Message
+  if (key === 'o') {
+    connection.write("Say: The Champ is Here!");
+  }
 };
 
 module.exports = setupInput;
